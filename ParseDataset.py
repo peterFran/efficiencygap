03ef0556aab1c57d1c32f57125cc569a67eaf064
Peter Meckiffe <pmeckif1@uk.ibm.com>
2016-12-07 23:05:17 +0000

    new things:

--- ParseDataset.py
+++ ParseDataset.py
@@ -1,6 +1,6 @@
 import pandas
 import numpy as np
-class Parser():
+class CongressElection():
     def parse_file(self, filename, sheetname=""):
         with open(filename) as f:
             ds = pandas.read_excel(f, sheetname=sheetname, parse_cols="C,D,I,K,P")
@@ -10,24 +10,31 @@ class Parser():
             ds = ds[~ds["GENERAL VOTES"].isin(["#"])]
             ds.rename(columns={'D':'DISTRICT'}, inplace=True)
             ds["GENERAL VOTES"] = np.where(ds["GENERAL VOTES"] == "Unopposed", 0, ds["GENERAL VOTES"])
+            self.df = ds
             return ds
-    def calculate_wasted(self, ds):
+    def calculate_wasted(self):
             # Work out wasted votes
+            ds = self.df
             grouped = ds.groupby(["STATE","DISTRICT"])
             ds["max"] = grouped["GENERAL VOTES"].transform(lambda x: max(x))
             ds["wasted"] = np.where(ds["max"] != ds["GENERAL VOTES"], ds["GENERAL VOTES"], -1)
             ds["runnerup"] = ds.groupby(["STATE","DISTRICT"])["wasted"].transform(lambda x: max(x))
+            # If effectively unopposed, the runner up should be 0.
+            # This presents an issue with the model, in that yes this demonstrates
+            ds["runnerup"] = np.where(ds["runnerup"] == -1, 0, ds["runnerup"])
             ds["win_count"] = np.where(ds["wasted"] == -1, 1, 0)
             ds["wasted"] = np.where(ds["wasted"] == -1, ds["GENERAL VOTES"] - ds["runnerup"], ds["wasted"])
             ds.drop('runnerup', 1, inplace=True)
             ds.drop('max', 1, inplace=True)
+            self.df = ds
             return ds
 
 
+
 if __name__ == '__main__':
-    p = Parser()
+    p = CongressElection()
     ds = p.parse_file("results14.xls", sheetname="2014 US House Results by State")
-    ds = p.calculate_wasted(ds)
+    ds = p.calculate_wasted()
     group = ds.groupby(["STATE", "PARTY"])
     ds["statewide_wasted_votes"] = group["wasted"].transform(lambda x: sum(x))
     ds["statewide_votes"] = group["GENERAL VOTES"].transform(lambda x: sum(x))
